f0224df0be1b2c8c32ed76d4bb548ed68b5ce69d
Chester Vincent <chetvincent@Chesters-MacBook-Pro-2.local>
2022-12-09 14:34:36 -0500

    test3

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+
 const app = express();
 
+app.use(express.json());
+app.use(cors());
 
+
+app.listen(3000, () => {
+    console.log();('listening....');
+});
+
+mongoose.connect('mongodb://localhost:27017/recipes')
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to mongodb');
+});
