af7403b4c442842bee8d598ff4d2ca5d5ba7b17a
Chester Vincent <chetvincent@Chesters-MacBook-Pro-2.local>
2022-12-09 14:26:02 -0500

    merge

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
 const app = express();
 
+
+
+
+
+app.listen(3000, () => {
+    console.log('The creature is listening...');
+})
