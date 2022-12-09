3627a7204ef1be844b9d22b1f0915f8ec4b04dc5
Chester Vincent <chetvincent@Chesters-MacBook-Pro-2.local>
2022-12-09 15:07:37 -0500

    seed db

---
+++ models/seed.js
@@ -0,0 +1,9 @@
+module.exports =[
+    {
+        "name": "Cheeseburger",
+        "time": "20 minutes",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cheeseburger.jpg/1200px-Cheeseburger.jpg",
+        "allergens": "dairy",
+        "featured": true,
+    }
+]
--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const Recipes = require('./models/recipe.js');
+const appRouter = require("./controllers/routes.js");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use("/", appRouter);
 
 
-app.listen(3000, () => {
-    console.log();('listening....');
-});
 
 mongoose.connect('mongodb://localhost:27017/recipes')
 
 mongoose.connection.once('open', () => {
     console.log('Connected to mongodb');
 });
+
+app.listen(3000, () => {
+    console.log();('listening....');
+});
+
+
+
+
+
+
+
+
+
+
+
+
