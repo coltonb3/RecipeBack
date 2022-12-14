d8b46c0f174ec51f549608da3098492f4f476ec3
Chester Vincent <chetvincent@chesters-mbp-2.mynetworksettings.com>
2022-12-14 16:01:48 -0500

    cors?

--- server.js
+++ server.js
@@ -26,12 +26,12 @@ if(process.env.PORT){
 };
 
 
-// const corsOptions ={
-//     origin:'http://localhost:3000',
-//     credentials:true,            //access-control-allow-credentials:true
-//     optionSuccessStatus:200
-// }
-// app.use(cors(corsOptions));
+const corsOptions ={
+    origin:'http://localhost:3000',
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 
 
